7dce2778363e4a140068e7e83a5e590f66c55885
toky rasolofo <toky rasolofo>
2017-01-12 00:27:54 +0100

    Création du lien pour voir l'article en GET

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
 
                 			<h1><?php echo $article->getTitle() ?></h1> <!-- Les 2 notations ('php echo' et =) se valent -->
                       		<p><?= $article->getContent() ?></p> <!-- syntaxe pour accede a un element d'un objet $objet->element-->
-                			<a class="btn btn-lg btn-primary" href="/article?id=<?php echo $article->getId(); ?>" role="button">Lire plus &raquo;</a></p>
+                			<a class="btn btn-lg btn-primary" href="articleShow.php?id=<?php echo $article->getId(); ?>" role="button">Lire plus &raquo;</a></p>
 
                     	</div>
 
